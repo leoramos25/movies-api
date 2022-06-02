8381cfaf84522eed1155d14da612949d44598f77
Leonardo Ramos de Sousa <leonardords@ciandt.com>
2022-06-02 15:30:39 -0300

    change connection string name

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<MovieContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("MovieDb")));
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<MovieContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("MovieConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
